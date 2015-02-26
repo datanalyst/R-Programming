43fa24c19546fca26d415b953bf08a82cfcc6926
datanalyst <datanalyst@users.noreply.github.com>
2015-02-26 22:21:25 +0100

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,20 +1,43 @@
+## Part 1
 
+# Set your working directory
+#setwd("")
 
-pollutantmean <- function(directory, pollutant ="nitrate", id = 1:332) {
-          #set the directory
-          path <- getwd()
-          
-          #get the file List in that directory
-          pmean <-numeric(0)
-               
-          for (i in id)  {
-            
-            csv <- read.csv(paste(path,"/", formatC(i, width = 3, flag = "0"),".csv",sep=""))                                        
-            pmean <- c(pmean, mean(csv[,pollutant],na.rm=TRUE))
-          }      
-          
-          #convert into data frame
-          data.frame(id=id,pmean=pmean)      
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+            #set the directory
+            path <- getwd()
+             
+            #get the file List in that directory
+            fileList <- list.files(path)
+
+            #extract the file names and store as numeric for comparison
+	    file.names <- as.numeric(sub("\\.csv$","",fileList))
+
+            #select files to be imported based on the user input or default
+            selected.files <- na.omit(fileList[match(id, file.names)])
+           
+            #import data (it is a list)
+            data <- lapply(file.path(path,selected.files),read.csv,header=TRUE)
+
+            #convert into data frame
+            data <- do.call(rbind.data.frame,data)
+
+            #create a subset by ID =id
+            subid <- subset(data, ID = id)
+                       
+            #compute the mean
+            mean(subid[, pollutant],na.rm = TRUE) 
+  	 
 }
 
+# Run tests
+pollutantmean("specdata", "nitrate", 23)
+## [1] 1.280833
+
+pollutantmean("specdata", "nitrate", 70:72)
+## [1] 1.706047
+
+pollutantmean("specdata", "sulfate", 1:10)
+## [1] 4.064128
 
