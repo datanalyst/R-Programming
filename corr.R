17d7aa0aad25aaa9ca40fa9eae8537d5675c783b
datanalyst <datanalyst@users.noreply.github.com>
2015-02-26 22:49:57 +0100

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,8 +1,31 @@
+
+# Write a function that takes a directory of data files and a threshold
+# for complete cases and calculates the correlation between sulfate and 
+# nitrate for monitor locations where the number of completely observed cases
+# (on all variables) is greater than the threshold. The function should return 
+# a vector of correlations for the monitors that meet the threshold requirement.
+# If no monitors meet the threshold requirement, then the function should return
+# a numeric vector of length 0. 
+
+# Set your working directory
+#setwd("")
+
 corr <- function(directory, threshold = 0) {
         
-        path <- getwd()
+        ## 'directory' is a character vector of length 1 indicating the location of
+        ## the CSV files
+        
+        ## 'threshold' is a numeric vector of length 1 indicating the number of
+        ## completely observed observations (on all variables) required to compute
+        ## the correlation between nitrate and sulfate; the default is 0
+        
+        # set the path
+        path <- "../specdata"
+                
+        # load the file complete.R
         source("complete.R")
         
+        ## Return a numeric vector of correlations
         com <- complete(path)
         data <- com[com$nobs > threshold, ]
         result <- numeric(0)
@@ -16,5 +39,15 @@ corr <- function(directory, threshold = 0) {
            
         result
 }
+
+# Test results
+cr <- corr("specdata", 150)
+head(cr)
+## [1] -0.01896 -0.14051 -0.04390 -0.06816 -0.12351 -0.07589
+
+summary(cr)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+## -0.2110 -0.0500  0.0946  0.1250  0.2680  0.7630
+
 
 
