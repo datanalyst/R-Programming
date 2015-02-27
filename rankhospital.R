0a04d5ecf8af9d6c6233dfc86531d636b9aac2dd
datanalyst <datanalyst@users.noreply.github.com>
2015-02-27 09:38:54 +0100

    Update rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,11 +1,12 @@
-#setwd("C:/Users/cg07510/Desktop/UTILI")
+# Set your working directory
+#setwd("")
 
 rankhospital <- function(state, outcome, num = "best") {
 
 		## Read outcome data
 		full_data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
 
-            ## Check that state and outcome are valid
+               ## Check that state and outcome are valid
 		column <- if (outcome == "heart attack") {
 			"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
 		} else if (outcome == "heart failure") {
@@ -21,7 +22,7 @@ rankhospital <- function(state, outcome, num = "best") {
 		stop("invalid state")	
 		}
 
-            ## Return hospital name in that state with the given rank 30-day death rate
+                ## Return hospital name in that state with the given rank 30-day death rate
 		data_for_state[,2] <- as.numeric(data_for_state[,2])
 		ordered_data_for_state <- order(data_for_state[column], data_for_state$Hospital.Name, na.last=NA)
 
@@ -37,12 +38,13 @@ rankhospital <- function(state, outcome, num = "best") {
 }
 
 
-#source("rankhospital.R")
-#rankhospital("TX", "heart failure", 4)
+# Run tests
+source("rankhospital.R")
+rankhospital("TX", "heart failure", 4)
 #[1] "DETAR HOSPITAL NAVARRO"
 
-#rankhospital("MD", "heart attack", "worst")
+rankhospital("MD", "heart attack", "worst")
 #[1] "HARFORD MEMORIAL HOSPITAL"
 
-#rankhospital("MN", "heart attack", 5000)
+rankhospital("MN", "heart attack", 5000)
 #[1] NA
